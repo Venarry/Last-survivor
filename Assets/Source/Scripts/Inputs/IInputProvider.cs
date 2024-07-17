88f42c7ba7d043c477a23821826eaaf347aac737
Venarry <venarry@mail.ru>
2024-07-17 21:21:44 +0300

    Change inputs provider

--- Assets/Source/Scripts/Inputs/IInputProvider.cs
+++ Assets/Source/Scripts/Inputs/IInputProvider.cs
@@ -2,5 +2,5 @@ using UnityEngine;
 
 public interface IInputProvider
 {
-    public Vector2 MoveDirection { get; }
+    public Vector3 MoveDirection { get; }
 }
--- Assets/Source/Scripts/Inputs/KeyboardInputProvider.cs
+++ Assets/Source/Scripts/Inputs/KeyboardInputProvider.cs
@@ -5,6 +5,6 @@ public class KeyboardInputProvider : IInputProvider
     private const string VectorHorizontal = "Horizontal";
     private const string VectorVertical = "Vertical";
 
-    public Vector2 MoveDirection =>
-        new Vector2(Input.GetAxisRaw(VectorHorizontal), Input.GetAxisRaw(VectorVertical)).normalized;
+    public Vector3 MoveDirection =>
+        new Vector3(Input.GetAxisRaw(VectorHorizontal), 0, Input.GetAxisRaw(VectorVertical)).normalized;
 }

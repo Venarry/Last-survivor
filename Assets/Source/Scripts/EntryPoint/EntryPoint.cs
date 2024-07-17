5e3ab8d8109c9bab2e9e51781f908f97393cf5d8
Venarry <venarry@mail.ru>
2024-07-17 21:29:59 +0300

    Carete mobile inputs provider

--- Assets/Source/Scripts/EntryPoint/EntryPoint.cs
+++ Assets/Source/Scripts/EntryPoint/EntryPoint.cs
@@ -4,6 +4,9 @@ public class EntryPoint : MonoBehaviour
 {
     private void Awake()
     {
+        bool isMobile = Application.isMobilePlatform;
+        Debug.Log(isMobile);
+
         IInputProvider inputProvider = new KeyboardInputProvider();
         PlayerFactory playerFactory = new(inputProvider);
 
---
+++ Assets/Source/Scripts/Inputs/MobileInputsProvider.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileInputsProvider : MonoBehaviour, IInputProvider
+{
+    public Vector3 MoveDirection => throw new System.NotImplementedException();
+
+
+}

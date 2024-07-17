512df6c0de2c3f05b2631cc58f55755c7366589d
Venarry <venarry@mail.ru>
2024-07-18 02:04:30 +0300

    Add target type and change itarget

--- Assets/Source/Scripts/Targets/ITarget.cs
+++ Assets/Source/Scripts/Targets/ITarget.cs
@@ -3,4 +3,7 @@ using UnityEngine;
 public interface ITarget
 {
     public Vector3 Position { get; }
+    public TargetType TargetType { get; }
+
+    public void TakeDamage();
 }
---
+++ Assets/Source/Scripts/Targets/TargetType.cs
@@ -0,0 +1,6 @@
+﻿public enum TargetType
+{
+    enemy,
+    wood,
+    ore,
+}

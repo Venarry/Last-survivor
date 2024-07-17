836fab5c710f55cab2b79b47a92b16749b3c91f3
Venarry <venarry@mail.ru>
2024-07-17 20:07:15 +0300

    Add player spawn

--- Assets/Source/Scripts/EntryPoint/EntryPoint.cs
+++ Assets/Source/Scripts/EntryPoint/EntryPoint.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EntryPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
+        IInputProvider inputProvider = new KeyboardInputProvider();
+        PlayerFactory playerFactory = new(inputProvider);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        playerFactory.Create(Vector3.zero);
     }
 }

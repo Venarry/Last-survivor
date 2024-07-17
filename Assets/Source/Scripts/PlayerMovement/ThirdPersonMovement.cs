05f7e055a6c6da23fb4ae0828cda84ca6359b1ed
Venarry <venarry@mail.ru>
2024-07-17 14:49:29 +0300

    Add player movement

---
+++ Assets/Source/Scripts/PlayerMovement/ThirdPersonMovement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class ThirdPersonMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed = 7f;
+    
+    private CharacterController _characterController;
+    private Vector3 _moveDirection;
+
+    public Vector3 Direction => _moveDirection;
+    public Vector3 Position => transform.position;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        _characterController.enabled = false;
+        transform.position = position;
+        _characterController.enabled = true;
+    }
+
+    public void SetDirection(Vector3 moveDirection)
+    {
+        _moveDirection = moveDirection;
+        _moveDirection = _moveDirection.normalized;
+
+        _moveDirection.y = -1f;
+    }
+
+    public void Move()
+    {
+        _characterController.Move(_speed * Time.deltaTime * _moveDirection);
+    }
+}

e0dddcd4abbb67cf487d8503f9e5b3774b6aa288
Venarry <venarry@mail.ru>
2024-08-29 05:30:38 +0300

    Add weapons visual

--- Assets/Source/Scripts/Skills/Skills/ThrowinAxes/ThrowingAxesSkill.cs
+++ Assets/Source/Scripts/Skills/Skills/ThrowinAxes/ThrowingAxesSkill.cs
@@ -16,6 +16,8 @@ public class ThrowingAxesSkill : SkillBehaviour
     private float _throwDistance = 14f;
     private float _axesCounter = 0;
 
+    private Vector3 SpawnPosition => _owner.position + new Vector3(0, 1f, 0);
+
     public ThrowingAxesSkill(
         TargetsProvider targetsProvider,
         ThrowingAxesFactory throwingAxesFactory,
@@ -36,12 +38,11 @@ public class ThrowingAxesSkill : SkillBehaviour
         if (_axesCounter > 0)
             return;
 
-        Vector3 spawnPoint = _owner.position + new Vector3(0, 1f, 0);
-        Ray ray = new(spawnPoint, Vector3.forward);
+        Ray ray = new(SpawnPosition, Vector3.forward);
 
         if (_targetsProvider.TryGetRayTargets(ray, _throwDistance, out _))
         {
-            _coroutineProvider.StartCoroutine(SpawnAxes(spawnPoint, CurrentLevel));
+            _coroutineProvider.StartCoroutine(SpawnAxes(CurrentLevel));
         }
     }
 
@@ -54,12 +55,12 @@ public class ThrowingAxesSkill : SkillBehaviour
         return "";
     }
 
-    private IEnumerator SpawnAxes(Vector3 position, float count)
+    private IEnumerator SpawnAxes(float count)
     {
         for (int i = 0; i < count; i++)
         {
             _axesCounter++;
-            Task<ThrowingAxe> axeTask = _throwingAxesFactory.Create(position, _throwDistance, _owner, _damageMultiplier);
+            Task<ThrowingAxe> axeTask = _throwingAxesFactory.Create(SpawnPosition, _throwDistance, _owner, _damageMultiplier);
 
             yield return axeTask;
 

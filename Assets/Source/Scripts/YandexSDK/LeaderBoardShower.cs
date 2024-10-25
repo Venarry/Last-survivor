54cdc52c38c130295d783c3e4729fa58dd4951e6
Venarry <venarry@mail.ru>
2024-10-25 19:48:41 +0300

    Add leadreboard with leaders score

--- Assets/Source/Scripts/YandexSDK/LeaderBoardShower.cs
+++ Assets/Source/Scripts/YandexSDK/LeaderBoardShower.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using YG;
+using YG.Utils.LB;
 
 public class LeaderBoardShower : MonoBehaviour
 {
@@ -10,17 +12,47 @@ public class LeaderBoardShower : MonoBehaviour
     [SerializeField] private Transform _leadersParent;
     [SerializeField] private Button _showMenuButton;
     [SerializeField] private Button _closeMenuButton;
+    [SerializeField] private UserLeaderScoreView _userLeaderScoreView;
+
+    private List<UserLeaderScoreView> _userLeaderScoreViews = new();
+
+    private void Awake()
+    {
+        _menu.SetActive(false);
+    }
 
     private void OnEnable()
     {
         _showMenuButton.onClick.AddListener(ShowMenu);
         _closeMenuButton.onClick.AddListener(CloseMenu);
+
+        YandexGame.onGetLeaderboard += OnLeaderboardGet;
     }
 
     private void OnDisable()
     {
         _showMenuButton.onClick.RemoveListener(ShowMenu);
         _closeMenuButton.onClick.RemoveListener(CloseMenu);
+
+        YandexGame.onGetLeaderboard -= OnLeaderboardGet;
+    }
+
+    private void OnLeaderboardGet(LBData data)
+    {
+        foreach (UserLeaderScoreView user in _userLeaderScoreViews)
+        {
+            Destroy(user.gameObject);
+        }
+
+        _userLeaderScoreViews.Clear();
+
+        foreach (LBPlayerData playerData in data.players)
+        {
+            UserLeaderScoreView playerView = Instantiate(_userLeaderScoreView, _leadersParent);
+            playerView.Set(playerData.rank, playerData.name, playerData.score);
+
+            _userLeaderScoreViews.Add(playerView);
+        }
     }
 
     private void ShowMenu()
---
+++ Assets/Source/Scripts/YandexSDK/UserLeaderScoreView.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UserLeaderScoreView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _rank;
+    [SerializeField] private TMP_Text _name;
+    [SerializeField] private TMP_Text _score;
+
+    public void Set(int rank, string name, int score)
+    {
+        _rank.text = rank.ToString();
+        _name.text = name;
+        _score.text = score.ToString();
+    }
+}
